cda9f544656f5fd3b607832dfef9530761c4f9a7
NestianEmanuelPaul <rafaelos_nep@yahoo.com>
2025-08-24 09:09:09 +0300

    add proxies and alternative sessions

--- app/utils/proxy_manager.py
+++ app/utils/proxy_manager.py
@@ -1,21 +1,18 @@
 import itertools
 import httpx
 
-# lista de proxy-uri (poți pune mai multe)
 proxies_list = [
     "socks5h://aepodqcp:rsbwdel9hpcq@136.0.207.84:6661",
     "socks5h://aepodqcp:rsbwdel9hpcq@216.10.27.159:6837"
 ]
 
-
-# Rotează proxy-urile la infinit
 proxy_cycle = itertools.cycle(proxies_list)
 
 async def get_next_working_proxy(start_proxy=None, test_url="https://httpbin.org/ip", timeout=5):
     # dacă ai primit un start_proxy, începi cu el
     if start_proxy:
         try:
-            async with httpx.AsyncClient(proxies={"http://": start_proxy, "https://": start_proxy}, timeout=timeout) as client:
+            async with httpx.AsyncClient(proxy=start_proxy, timeout=timeout) as client:
                 r = await client.get(test_url)
                 if r.status_code == 200:
                     return start_proxy
@@ -26,12 +23,10 @@ async def get_next_working_proxy(start_proxy=None, test_url="https://httpbin.org
     for _ in range(len(proxies_list)):
         proxy = next(proxy_cycle)
         try:
-            async with httpx.AsyncClient(proxies={"http://": proxy, "https://": proxy}, timeout=timeout) as client:
+            async with httpx.AsyncClient(proxy=proxy, timeout=timeout) as client:
                 r = await client.get(test_url)
                 if r.status_code == 200:
                     return proxy
         except Exception:
             continue
     return None
-
-
